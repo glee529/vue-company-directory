33e005a7626435ea34f8fe268395942cd22af50c
glee529 <290x290@gmail.com>
2023-02-10 00:43:17 -0600

    Added cards

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,10 +1,11 @@
 <script setup>
 import MainSearch from './MainSearch.vue'
+import MainCards from './MainCards.vue'
 </script>
 <template>
   <div class="wrapper">
     <MainSearch />
-    <h1>Hello there</h1>
+    <MainCards />
   </div>
 </template>
 
