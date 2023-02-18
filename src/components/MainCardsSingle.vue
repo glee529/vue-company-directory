8b17be90fa3c437f94b39146f3cc3f9ea44bacae
glee529 <290x290@gmail.com>
2023-02-18 00:04:40 -0600

    finished

--- src/components/MainCardsSingle.vue
+++ src/components/MainCardsSingle.vue
@@ -1,11 +1,31 @@
 <script setup>
+import { ref } from 'vue'
 import { faker } from '@faker-js/faker'
-const firstName = faker.name.firstName()
-const lastName = faker.name.lastName()
-const fullName = `${firstName} ${lastName}`
+import useAPI from '@/composables/useAPI'
+const { getDepartment } = useAPI()
 const selectCard = () => {
-  console.log(`${fullName} selected`)
+  console.log(`${props.employee.name} selected`)
 }
+const props = defineProps({
+  employee: {
+    type: Object,
+    required: true,
+    default: () => {
+      return {
+        createdAt: '2022-01-01',
+        departmentId: '123',
+        email: 'john.doe@example.com',
+        employeeId: '123',
+        name: 'John Doe',
+        quote: 'Really Cool quote',
+        title: 'Position',
+        updatedAt: '2022-01-01',
+      }
+    },
+  },
+})
+const departmentResponse = await getDepartment(props.employee.departmentId)
+const department = ref(departmentResponse)
 </script>
 
 <template>
@@ -14,9 +34,9 @@ const selectCard = () => {
       <img :src="faker.internet.avatar()" alt="" srcset="" />
     </div>
     <div class="card-details">
-      <p class="card-details-name">{{ fullName }}</p>
-      <p class="card-details-job">{{ faker.name.jobTitle() }}, {{ faker.name.jobArea() }}</p>
-      <p class="card-details-quote">"{{ faker.lorem.paragraph() }}"</p>
+      <p class="card-details-name">{{ props.employee.name }}</p>
+      <p class="card-details-job">{{ props.employee.title }}, {{ department.name }}</p>
+      <p class="card-details-quote">"{{ props.employee.quote }}"</p>
     </div>
   </div>
 </template>
@@ -32,10 +52,10 @@ const selectCard = () => {
   &-details {
     @apply flex flex-col gap-2  pt-6 text-center;
     &-name {
-      @apply text-3xl font-bold  tracking-wider text-slate-800;
+      @apply text-3xl font-thin  tracking-wider text-slate-800;
     }
     &-job {
-      @apply -mt-2 text-xs font-bold text-blue-700;
+      @apply -mt-2 text-xs font-bold text-yellow-700;
     }
     &-quote {
       @apply pt-4 text-lg italic text-slate-800;
